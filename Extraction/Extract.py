2a2e45fd9bec670729e8f3051464aeee24925011
Gianluca Lambruschini <gianluca.lambruschini@gmail.com>
2025-08-05 20:21:56 -0300

    Realicé una modificación donde eliminé el anterior valor de la funcion df por file_path

--- Extraction/Extract.py
+++ Extraction/Extract.py
@@ -1,5 +1,5 @@
 from Config import olympics_raw_path
 import pandas as pd
 
-def extract_from_csv(df):
-    return pd.read_csv(df)
+def extract_from_csv(file_path):
+    return pd.read_csv(file_path)
