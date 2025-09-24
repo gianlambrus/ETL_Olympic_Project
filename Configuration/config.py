5fc44e1dbb9d51b7e4859fb3bc0eb761a964f2ef
Gianluca Lambruschini <gianluca.lambruschini@gmail.com>
2025-09-24 19:04:49 -0300

    Agregue el archivo dag, que permite la conexión con airflow.

--- Configuration/config.py
+++
@@ -1,6 +0,0 @@
-datalake_base = "D:/Practica_ETL/datalake"
-
-olympic_raw_path = "D:/Practica_ETL/Base/olympics.csv"
-bronze_path = f"{datalake_base}/bronze/olympics"
-silver_path = f"{datalake_base}/silver/olympics"
-gold_path = f"{datalake_base}/gold/olympics"
