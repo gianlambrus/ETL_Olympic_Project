e6cac128df43f002b3626c458d270b9be3d3046e
Gianluca Lambruschini <gianluca.lambruschini@gmail.com>
2025-09-27 20:20:12 +0000

    Agregue las funciones logger para extract

--- extraction/extract.py
+++ extraction/extract.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from logger import get_logger
+
+logger = get_logger(__name__)
 
 def extract_from_csv(file_path):
+    logger.info(f"Extrayendo {file_path}")
     return pd.read_csv(file_path)
