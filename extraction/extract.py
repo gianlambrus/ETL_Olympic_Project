479ea76fd39a83d8bc0d1dc52e0297af0c2bce80
Gianluca Lambruschini <gianluca.lambruschini@gmail.com>
2025-10-01 23:56:28 +0000

    Cree un docstring

--- extraction/extract.py
+++ extraction/extract.py
@@ -4,5 +4,7 @@ from logger import get_logger
 logger = get_logger(__name__)
 
 def extract_from_csv(file_path):
-    logger.info(f"Extrayendo {file_path}")
+    #ES: Extrae el archivo csv
+    #EN: Extracts a csv file
+    logger.debug(f"Extrayendo {file_path}")
     return pd.read_csv(file_path)
