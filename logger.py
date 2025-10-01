bf14ae0775cd674f68ac0800126eb7c2d4b7db86
Gianluca Lambruschini <gianluca.lambruschini@gmail.com>
2025-10-01 22:12:14 +0000

    Cree una funcion mas robusta para la creacion de loggers

--- logger.py
+++ logger.py
@@ -1,12 +1,25 @@
 import logging
+from logging import Logger
+from pathlib import Path
 
-def get_logger(name: str):
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-        handlers=[
-            logging.FileHandler("extraction.log"),
-            logging.StreamHandler()
-        ]
-    )
-    return logging.getLogger(name)
+log_file = Path(__file__).resolve().parent / "etl.log"
+
+def get_logger(name: str) -> Logger:
+    logger = logging.getLogger(name)
+    if logger.handlers:
+        return logger
+    logger.setLevel(logging.INFO)
+    fmt = logging.Formatter("%(asctime)s [%(levelname)s] %[name]s: %(message)s")
+
+    fh = logging.FileHandler(log_file, encoding="utf-8")
+    fh.setLevel(logging.INFO)
+    fh.setFormatter(fmt)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(fmt)
+
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    logger.propagate = False
+    return logger
